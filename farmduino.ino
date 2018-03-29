f0b80f3daadff8fd213e62125f105f6026120fe8
Christian Verdú <me@chverma.com>
2018-03-29 18:27:58 +0200

    Refractorize main code

--- farmduino.ino
+++ farmduino.ino
@@ -1,77 +1,75 @@
 #include "farmduino.h"
 
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib. To change pins for Software Serial, use the two lines in GSM.cpp.
+RTC_DS1307 rtc;
 
 // create objects for each command
 WaterOnCommand wOnCommand;
 WaterOffCommand wOffCommand;
-ApiCheckCommand apiCheckCommand;
-TimedCommand *tCommands[30];
+// Define output variable for ApiCheckCommand
+int apiChecked=0;
+ApiCheckCommand apiCheckCommand(&apiChecked);
 
-RTC_DS1307 rtc; // Date and time functions using a DS1307 RTC connected via I2C and Wire lib. To change pins for Software Serial, use the two lines in GSM.cpp.
+// Define output variables for ApiWaterCommand
+String cronStrWaterOn = "-2.*.*.*.*.*";
+String cronStrWaterOff = "-2.*.*.*.*.*";
+int cronLinesUpdated = 0;
+ApiWaterCommand apiGetWaterCommand(&cronLinesUpdated, &cronStrWaterOn, &cronStrWaterOff);
+
+// Define a TimedCommand array with a static size
+TimedCommand *tCommands[4];
+// Define the cron object
 Cron cron(tCommands, 0);
-InetGSM inet;
-char msg[500];
-int numdata;
-char inSerial[50];
-int i=0;
-boolean started=false;
-TimedCommand command("*.*.20.*.*.*", &apiCheckCommand, SERVO_PIN, true);
-TimedCommand command1("*.*.8.*.*.*", &wOnCommand, SERVO_PIN, true);
-TimedCommand command2("20.*.*.*.*.*",&wOnCommand, SERVO_PIN);
-TimedCommand command3("25.*.*.*.*.*",&wOffCommand, SERVO_PIN);
+
+// Declare commands to be modified by later by commApiGetWater.
+// NOTE: The value -2 has been set to not match the current time. So, it won't be executed until the cron strings has been modified.
+TimedCommand commApiCheck("-2.*.[7,8,9,10,11,12,13,14,15,14,17,19,23,3].*.*.*", &apiCheckCommand, SERVO_PIN, true, 2);
+TimedCommand commApiGetWater("*.*.[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23].*.*.*", &apiGetWaterCommand, SERVO_PIN, true, 2);
+TimedCommand commWaterOn("-2.*.*.*.*.*", &wOnCommand, SERVO_PIN, true, 2);
+TimedCommand commWaterOff("-2.*.*.*.*.*",&wOffCommand, SERVO_PIN, true, 2);
+
 void setup()
 {
-     // initCron();
-     Serial.begin(9600);
-     if (! rtc.begin()) {
-       Serial.println("Couldn't find RTC");
-       while (1);
-     }
-     
-     cron.addTimedCommand(&command);
-     cron.addTimedCommand(&command2);
-     cron.addTimedCommand(&command3);
-     int *i = 0;
-     //Serial.println(typeid(i).name());
-        // sets the time to be the date and time of the compilation
-        //cron.setTime(rtc.now());
-        // prints the current time
-        
-        cron.printTime();
-     if (! rtc.isrunning()) {
-        Serial.println("RTC is NOT running!");
-        // following line sets the RTC to the date & time this sketch was compiled
-        //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-     }
+  Serial.begin(9600);
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+  }
+  cron.printTime();
+  // Add cron API commands
+  cron.addTimedCommand(&commApiCheck);
+  cron.addTimedCommand(&commApiGetWater);
+  // Add cron WATER ON/OFF commands
+  cron.addTimedCommand(&commWaterOn);
+  cron.addTimedCommand(&commWaterOff);
+  cron.printTime();
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 
-//    powerUpGSM();
-//    initGSM();
-    
-     
-     
+  delay(1000);
 };
 
-
+int waitForSleep = 0;
 void loop()
 {
-    cron.loop();
-    cron.printTime();
-//    DateTime now = rtc.now();
-//    
-//    Serial.print(now.year(), DEC);
-//    Serial.print('/');
-//    Serial.print(now.month(), DEC);
-//    Serial.print('/');
-//    Serial.print(now.day(), DEC);
-//    Serial.print(' ');
-//    Serial.print(now.hour(), DEC);
-//    Serial.print(':');
-//    Serial.print(now.minute(), DEC);
-//    Serial.print(':');
-//    Serial.print(now.second(), DEC);
-//    Serial.println();
-    //delay(1000);
-    //sleep(50);     
+  cron.loop();
+  cron.printTime();
+    
+  if (cronLinesUpdated) {
+    Serial.println("Cron lines updated!");
+    updateCronStrings();
+    cronLinesUpdated = 0;
+    waitForSleep = 20;
+   }
+   
+   if (waitForSleep == 0){
+      //sleep(30*60);
+      waitForSleep++;
+    } else {
+      waitForSleep--;
+    }
 }
 
 
@@ -92,88 +90,12 @@ void sleep(int sec) {
     LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
     sec -= 1;
   }
-  //sendSignal();
-}
-void initGSM()
-{
-  Serial.println("GSM Shield testing.");
-  //Start configuration of shield with baudrate.
-  //For http uses is raccomanded to use 4800 or slower.
-  if (gsm.begin(9600)) {
-    Serial.println("GSM READY");
-    started=true;
-  } else 
-    Serial.println("GSM IDLE");
-
-  if(started) {
-   //GPRS attach, put in order APN, username and password.
-   //If no needed auth let them blank.
-   if (inet.attachGPRS("orangeworld", "", ""))
-    Serial.println("GPRS ATTACHED");
-   else 
-    Serial.println("GPRS ERROR");
-    
-   delay(1000);
-   }
 }
 
-
-void powerUpGSM()
-{
-  pinMode(44, OUTPUT);
-  digitalWrite(44, LOW);
-  delay(1000);
-  digitalWrite(44, HIGH);
-  delay(2000);
-  digitalWrite(44, LOW);
-  Serial.println("POWered");
-  delay(3000);
-  delay(10000);
+void updateCronStrings(){
+  Serial.println("cronStrWaterOn: "+cronStrWaterOn);
+  Serial.println("cronStrWaterOff: "+cronStrWaterOff);
+  commWaterOn.setCronString(cronStrWaterOn);
+  commWaterOff.setCronString(cronStrWaterOff);
 }
 
-
-
-void httpGetActionDone() {
-          //TCP Client GET, send a GET request to the server and
-          //save the reply.
-          int len = 97;
-          numdata=inet.httpGET(FARMDUINO_SERVER, 80, "/rest/v1/api/check", msg, 500);
-          //Print the results.
-          Serial.println("Number of data received:");
-          Serial.println(numdata);
-          Serial.println("Data received:");
-          // Find where the json starts and ends
-          int ind1 = String(msg).indexOf('{');
-          int ind2 = String(msg).indexOf('}');
-          // Get the substring
-          String finalstring = String(msg).substring(ind1, ind2+1);
-          // Create the json object
-          DynamicJsonBuffer jsonBuffer;
-          JsonObject& root = jsonBuffer.parseObject(finalstring);
-          Serial.println(finalstring);
-          Serial.println("AHI VA");
-          String prova = root[String("from")];
-          Serial.println(prova);
-}
-
-void httpGetWaterAction() {
-          //TCP Client GET, send a GET request to the server and
-          //save the reply.
-          numdata=inet.httpGET(FARMDUINO_SERVER, 80, API_CHECK, msg, 500);
-          //Print the results.
-          Serial.println("Number of data received:");
-          Serial.println(numdata);
-          Serial.println("Data received:");
-          // Find where the json starts and ends
-          int ind1 = String(msg).indexOf('{');
-          int ind2 = String(msg).indexOf('}');
-          // Get the substring
-          String finalstring = String(msg).substring(ind1, ind2+1);
-          // Create the json object
-          DynamicJsonBuffer jsonBuffer;
-          JsonObject& root = jsonBuffer.parseObject(finalstring);
-          Serial.println(finalstring);
-          Serial.println("AHI VA");
-          String prova = root[String("from")];
-          Serial.println(prova);
-}
